efc3498f13c6bee99f28e1b6b51a7ff96ca779e7
gsg211 <goreagabi8@gmail.com>
2025-11-13 15:00:43 +0200

    StateMachine

--- src/State Machine/StateMachine.py
+++ src/State Machine/StateMachine.py
@@ -1,28 +1,47 @@
+from __future__ import annotations
+
 from abc import *
 
 
 class StateMachine:
-    def __init__(self, startState):
+    def __init__(self, startState:IState):
         self.state = startState
+        self.state.set_context(self)
 
     def action(self):
         self.state.action()
 
-    def changeState(self, newState):
-        self.state = newState
+    def change_state(self, new_state:IState):
+        self.state = new_state
+        self.state.set_context(self)
+
 
 class IState(ABC):
     @abstractmethod
     def action(self):
         pass
 
+    @abstractmethod
+    def set_context(self, context: StateMachine):
+        pass
 
-class State(IState, ABC):
-    def __init__(self, stateMachine: StateMachine):
-        self.context = stateMachine
+    @abstractmethod
+    def change_state(self, new_state: IState):
+        pass
 
-    def changeState(self,newState):
-        self.context.changeState(newState)
+class ConcreteState(IState, ABC):
+    def __init__(self):
+        self.context = None
 
+    def set_context(self, context:StateMachine):
+        self.context=context
 
+    def change_state(self, new_state:IState):
+        if self.context is None:
+            raise RuntimeError("State has no context assigned.")
+        self.context.change_state(new_state)
+
+    @abstractmethod
+    def action(self):
+        pass
 
