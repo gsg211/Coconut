e17197fc4f927a6d4d4ba78b0f535ac4802c05da
gsg211 <goreagabi8@gmail.com>
2025-11-16 17:47:35 +0200

    incomplete sliding window

--- src/SlidingWindow/ReceivingWindow.py
+++ src/SlidingWindow/ReceivingWindow.py
@@ -1,3 +1,7 @@
+from token import NUMBER
+import random
+import time
+
 import protocol.SocketManager as sm
 from itertools import islice
 import UDP_packet as udp
@@ -25,6 +29,79 @@ class ReceivingWindow:
         self.__manager.set_peer_data(sender_address, sender_port)
         self.__time_out_interval = time_out_interval
         self.__packet_loss_chance = packet_loss_chance
-        self.window = [None] * window_size
         self.packet_list: list[udp.UDP_Packet] = list()
+        self.done_transmission=False
+
+    def send_ACK(self, sequence_number):
+        ack_packet=udp.UDP_Packet(d.Flow_Header.H_ACK,sequence_number,'')
+        self.__manager.q_snd_put(ack_packet.get_full_message())
+
+    def send_NAK(self, sequence_number):
+        nak_packet = udp.UDP_Packet(d.Flow_Header.H_NAK, sequence_number, '')
+        self.__manager.q_snd_put(nak_packet.get_full_message())
+
+    # decided if packet gets lost (used to simulate real packet  loss)
+    def will_lose(self) -> bool:
+        return random.random() < self.__packet_loss_chance
+
+    def listen(self):
+        self.__manager.start()
+        window: list[udp.UDP_Packet]
+        buffer: dict[int, udp.UDP_Packet] = {}
+        expected_number=1
+        total_number_of_packets=-1
+        while True:
+            expected_packet = self.__manager.q_rcv_get()
+            if expected_packet is None:
+                time.sleep(0.01)
+                continue
+
+            raw_packet, (addr, port) = expected_packet
+            pkt = udp.UDP_Packet.__new__(udp.UDP_Packet)
+            pkt.init_from_full_message(bytearray(raw_packet))
+            pkt.print_everything_decoded()
+
+            if pkt.get_custom_header() == d.Flow_Header.H_DONE:
+                total_number_of_packets=pkt.get_seq_nr()
+                continue
+
+            sequence_number = pkt.get_seq_nr()
+            if sequence_number == expected_number:
+                self.packet_list.append(pkt)
+                self.send_ACK(sequence_number)
+                expected_number+=1
+
+                while expected_number in buffer:
+                    expected_packet = buffer[expected_number]
+                    self.packet_list.append(expected_packet)
+                    buffer.pop(expected_number)
+                    expected_number += 1
+
+            elif sequence_number > expected_number:
+                buffer[sequence_number]=pkt
+                for missing_number in range(expected_number,sequence_number):
+                    self.send_NAK(missing_number)
+
+            else:
+                pass
+
+            if len(self.packet_list) == total_number_of_packets - 1 :
+                self.done_transmission=True
+                return
+
+    def get_data(self) -> str | None:
+        if self.done_transmission:
+            return ''.join(packet.get_payload() for packet in self.packet_list)
+        return None
+
+
+
+if __name__ == "__main__":
+    sender = ReceivingWindow()
+    sender.listen()
+    while True:
+        string=sender.get_data()
+        if string is not None:
+            print(string)
+            break
 
--- src/SlidingWindow/SendingWindow.py
+++ src/SlidingWindow/SendingWindow.py
@@ -1,7 +1,11 @@
+import time
+from operator import truediv
+
 import protocol.SocketManager as sm
 from itertools import islice
 import UDP_packet as udp
 import defines as d
+import random
 
 """
 DEFAULT_PORT_A => SENDER
@@ -27,37 +31,72 @@ class SendingWindow:
         self.window= [None] * window_size
         self.packet_list : list[udp.UDP_Packet]=list()
 
-    def split_input(self,data:str):
+    #splits the input string into multiple strings of a max length (__packet_data_size)
+    def split_input(self,data:str) -> list[str]:
         it = iter(data)
         string_list = [''.join(islice(it, self.__packet_data_size)) \
                for _ in range((len(data) + self.__packet_data_size - 1) // self.__packet_data_size)]
         return string_list
 
+    #converts the string into a list of packets
     def convert_data_to_packets(self, data:str):
         string_list=self.split_input(data)
-        print(string_list)
         sequence_counter=1
         for string in string_list:
             packet_to_send=udp.UDP_Packet(d.Operation_Header.H_DATA,sequence_counter,string)
             packet_to_send.print_payload_decoded()
             self.packet_list.append(packet_to_send)
             sequence_counter+=1
-        print(len(self.packet_list))
 
+    #decided if packet gets lost (used to simulate real packet  loss)
+    def will_lose(self) -> bool:
+        return random.random() < self.__packet_loss_chance
 
+    def send_H_DONE(self,sequence_number):
+        nak_packet = udp.UDP_Packet(d.Flow_Header.H_NAK, sequence_number, '')
+        self.__manager.q_snd_put(nak_packet.get_full_message())
+
+    #sends the data using the sliding window protocol
     def send(self, data: str):
 
         self.convert_data_to_packets(data)
         self.__manager.start()
-
+        window: list[udp.UDP_Packet] =list()
         current_window_position=0
         while current_window_position + self.__window_size <= len(self.packet_list):
 
             for position in range(current_window_position,current_window_position + self.__window_size):
                 packet_to_send=self.packet_list[position]
+                window.append(packet_to_send)
                 self.__manager.q_snd_put(packet_to_send.get_full_message())
 
+            while window: #wait for ACKS
+                print(window)
+                packet = self.__manager.q_rcv_get()
+                if packet is None:
+                    time.sleep(0.01)
+                    continue
+
+                raw_packet, (addr, port) = packet
+                pkt = udp.UDP_Packet.__new__(udp.UDP_Packet)
+                pkt.init_from_full_message(bytearray(raw_packet))
+                pkt.print_everything_decoded()
+
+                if pkt.get_custom_header() == d.Flow_Header.H_NAK:
+                    seq=pkt.get_seq_nr()
+                    for packet in window:
+                        if packet.get_seq_nr() == seq:
+                            self.__manager.q_snd_put(packet.get_full_message())
+                elif pkt.get_custom_header() == d.Flow_Header.H_ACK:
+                    seq = pkt.get_seq_nr()
+                    for packet in window:
+                        if packet.get_seq_nr() == seq:
+                            window.remove(packet)
+
             current_window_position+=self.__window_size
+
+        self.send_H_DONE(len(self.packet_list)+1)
+
 
 
 
