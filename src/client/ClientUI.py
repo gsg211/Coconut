1243e690920b6885515f90f79f67efe765799964
gsg211 <goreagabi8@gmail.com>
2026-01-11 16:25:18 +0200

    Welcome message

--- src/client/ClientUI.py
+++ src/client/ClientUI.py
@@ -2,9 +2,9 @@ import sys
 import json
 import os
 
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtCore import Qt, QSize
-from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtGui import QIcon, QFont, QPixmap
 from PyQt5.QtWidgets import *
 
 import defines as d
@@ -26,43 +26,80 @@ class ClientWindow(QWidget):
 
     def init_ui(self):
         self.setStyleSheet("color: white; background-color: #1e2124;")
+
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignTop)
+        layout.setSpacing(8)
+        layout.setContentsMargins(25, 15, 25, 15)
 
-        top_container = QWidget()
-        top_layout = QHBoxLayout(top_container)
-        top_layout.setContentsMargins(0, 0, 0, 0)
+        header_container = QWidget()
+        header_layout = QHBoxLayout(header_container)
+        header_layout.setContentsMargins(0, 0, 0, 0)
+        header_layout.addStretch(1)
 
-        self.output_label = QLabel("Output:")
-        self.output_label.setFont(QFont("Consolas", 12))
+        welcome_container = QWidget()
+        welcome_layout = QHBoxLayout(welcome_container)
+        welcome_layout.setContentsMargins(0, 0, 0, 0)
+        welcome_layout.setSpacing(15)
+
+        welcome_label = QLabel("Welcome to Coconut")
+        welcome_label.setFont(QFont("Consolas", 26, QFont.Bold))
+
+        coconut_icon = QLabel()
+        pixmap = QPixmap(f"{get_resource_path()}/Icons/Coconut.png")
+        scaled_pixmap = pixmap.scaled(60, 60, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        coconut_icon.setPixmap(scaled_pixmap)
+
+        welcome_layout.addWidget(welcome_label)
+        welcome_layout.addWidget(coconut_icon)
+
+        header_layout.addWidget(welcome_container)
+        header_layout.addStretch(1)
 
         self.config_btn = QPushButton()
         self.config_btn.setIcon(QIcon(f"{get_resource_path()}/Icons/settings.png"))
         self.config_btn.setIconSize(QSize(40, 40))
-        self.config_btn.setFixedSize(60, 60)
+        self.config_btn.setFixedSize(55, 55)
         self.config_btn.setStyleSheet(load_stylesheet("ImageBtn.css"))
         self.config_btn.setCursor(Qt.PointingHandCursor)
         self.config_btn.clicked.connect(self.switch_to_config.emit)
+        header_layout.addWidget(self.config_btn)
 
-        top_layout.addWidget(self.output_label)
-        top_layout.addStretch()
-        top_layout.addWidget(self.config_btn)
 
         self.output_text = QTextBrowser()
         self.output_text.setFont(QFont("Consolas", 12))
-        self.output_text.setStyleSheet("background-color: #36393e; color: white; border: none;")
+        self.output_text.setStyleSheet("background-color: #36393e; color: white; border: 2px solid #444; border-radius: 8px; padding: 10px;")
+        self.output_text.setMinimumHeight(180)
+
 
         self.source_path_label = QLabel("Source Path:")
-        self.source_path_label.setFont(QFont("Consolas", 12))
+        self.source_path_label.setFont(QFont("Consolas", 13, QFont.Bold))
+
         self.source_path_text_box = QLineEdit()
-        self.source_path_text_box.setMinimumHeight(40)
+        self.source_path_text_box.setMinimumHeight(45)
+        self.source_path_text_box.setFont(QFont("Consolas", 12))
         self.source_path_text_box.setStyleSheet(load_stylesheet("TextBox.css"))
 
         self.destination_path_label = QLabel("Destination Path:")
-        self.destination_path_label.setFont(QFont("Consolas", 12))
+        self.destination_path_label.setFont(QFont("Consolas", 13, QFont.Bold))
+
         self.destination_path_text_box = QLineEdit()
+        self.destination_path_text_box.setMinimumHeight(45)
+        self.destination_path_text_box.setFont(QFont("Consolas", 12))
         self.destination_path_text_box.setStyleSheet(load_stylesheet("TextBox.css"))
-        self.destination_path_text_box.setMinimumHeight(40)
+
+
+        layout.addWidget(header_container)
+        layout.addWidget(self.output_text)
+
+        layout.addSpacing(5)
+
+        layout.addWidget(self.source_path_label)
+        layout.addWidget(self.source_path_text_box)
+        layout.addWidget(self.destination_path_label)
+        layout.addWidget(self.destination_path_text_box)
+
+        layout.addSpacing(10)
 
         self.view_btn = self.create_button("View Tree", "Icons/view tree.png", self.view_tree)
         self.create_btn = self.create_button("Create New File", "Icons/create.png", self.create_file)
@@ -71,12 +108,6 @@ class ClientWindow(QWidget):
         self.download_btn = self.create_button("Download File", "Icons/download.png", self.download_file)
         self.upload_btn = self.create_button("Upload File", "Icons/upload.png", self.upload_file)
 
-        layout.addWidget(top_container)
-        layout.addWidget(self.output_text)
-        layout.addWidget(self.source_path_label)
-        layout.addWidget(self.source_path_text_box)
-        layout.addWidget(self.destination_path_label)
-        layout.addWidget(self.destination_path_text_box)
         layout.addWidget(self.view_btn)
         layout.addWidget(self.create_btn)
         layout.addWidget(self.delete_btn)
@@ -186,7 +217,7 @@ class MainWindowContainer(QMainWindow):
     def __init__(self, client):
         super().__init__()
         self.client = client
-        self.setWindowTitle("Client UI")
+        self.setWindowTitle("Coconut file transfer")
         self.setGeometry(100, 100, 800, 800)
         self.setStyleSheet("background-color: #1e2124;")
 
